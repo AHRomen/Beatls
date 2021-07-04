4ec460378f90fa3fff83712cca4accb92350cce8
AHRomen <73189275+AHRomen@users.noreply.github.com>
2021-07-04 21:42:04 +0600

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,28 @@
+
+
+// $(document).ready(function(){
+//     $("#navdesign").click(function(){
+//       $(".navigation").toggleClass("mynavi");
+//     });
+//   });
+// $("#infoToggler").click(function() {
+//     $(this).find('img').toggle();
+// });
+
+$(document).ready(function(){
+    $("#navdesign").click(function(){
+        $(".navigation").toggleClass("mynavi");
+        $(".navbar-brand").find('img').toggle();
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
